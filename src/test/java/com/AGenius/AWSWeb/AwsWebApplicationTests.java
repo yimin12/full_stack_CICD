5a4f051bac9ed62680ae9ae133d1b40400c3e63c
yimin <hymlaucs@gmail.com>
2022-03-27 11:37:41 -0400

    "update Github Action deployment configuration"

--- src/test/java/com/AGenius/AWSWeb/AwsWebApplicationTests.java
+++ src/test/java/com/AGenius/AWSWeb/AwsWebApplicationTests.java
@@ -1,5 +1,6 @@
 package com.AGenius.AWSWeb;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class AwsWebApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.fail("Ooops, test failed");
 	}
 
 }
